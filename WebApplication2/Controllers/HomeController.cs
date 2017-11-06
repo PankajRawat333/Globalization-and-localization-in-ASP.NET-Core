25abfbb41a242e2051ada3e3727d5d25a325052d
SAPIENT\prawa5 <pankaj.rawat@sapient.com>
2017-11-06 18:57:17 +0530

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["CurrentCulture"] = System.Threading.Thread.CurrentThread.CurrentCulture;
+            ViewData["CurrentUICulture"] = System.Threading.Thread.CurrentThread.CurrentUICulture;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return RedirectToAction("Index");
+        }
+    }
+}
